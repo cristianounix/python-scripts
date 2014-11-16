12c38e481040112cfee8d77029cd146d292a2ec4
cristianounix <cristianounix@gmail.com>
2014-11-16 00:51:25 -0200

    URL site

--- get-page.py
+++ get-page.py
@@ -8,20 +8,20 @@ def request_url(thread_name, start, end):
     for x in range(start, end):
         c = pycurl.Curl()
         print(str(thread_name)+"Processando --> "+str(x))
-        c.setopt(c.URL, 'http://www.tudogostoso.com.br/receita/print_recipe.php?recipe_id='+str(x))
+        c.setopt(c.URL, 'http://www.site.com.br/?id='+str(x))
         c.setopt(c.WRITEDATA, buffer)
         c.perform()
         c.close()
         body = buffer.getvalue()
-        f = open('receita-'+str(x)+'.html','w')
+        f = open('pagina-'+str(x)+'.html','w')
         f.write(body)
         f.close()
 
 
 
 try:
-    thread.start_new_thread( request_url, ("Thread(19-8998)", 19,8998) )
-    thread.start_new_thread( request_url, ("Thread(8998-17999)", 8998,17999) )
+    thread.start_new_thread( request_url, ("Thread(1)", 19,8998) )
+    thread.start_new_thread( request_url, ("Thread(2)", 8998,17999) )
 
 except:
     print "Error: unable to start thread"
